c61a0aeb2f9828c5dba7b91ca4d8de95c7d68973
Agam Brahma <git@agambrahma.com>
2022-11-27 14:01:08 -0800

    Refactor out sample model code

--- wholeness/model.go
+++ wholeness/model.go
@@ -13,18 +13,3 @@ func (m *SimpleModel) BigBang(w World) {
 		w.Add(agent, w.GetRandomPosition())
 	}
 }
-
-func NewNoOpModel() SimpleModel {
-	return SimpleModel{
-		agents: []Agent{&noOpAgent{}, &noOpAgent{}},
-	}
-}
-
-func NewMovingModel() SimpleModel {
-	return SimpleModel{
-		agents: []Agent{
-			&constantMoveAgent{change: Position{Y: 1, X: 0}},
-			&constantMoveAgent{change: Position{Y: 0, X: -1}},
-		},
-	}
-}
--- wholeness/testagents.go
+++ wholeness/testagents.go
@@ -8,6 +8,12 @@ func (a *noOpAgent) Init(_ Position) {}
 
 func (a *noOpAgent) Tick(_ AgentContext) {}
 
+func NewNoOpModel() SimpleModel {
+	return SimpleModel{
+		agents: []Agent{&noOpAgent{}, &noOpAgent{}},
+	}
+}
+
 //////////////////////////////////////// Constant-Movement
 type constantMoveAgent struct {
 	current Position
@@ -25,4 +31,13 @@ func (a *constantMoveAgent) Tick(ctx AgentContext) {
 	}
 	ctx.Move(a.current, newPosition)
 	a.current = newPosition
+}
+
+func NewMovingModel() SimpleModel {
+	return SimpleModel{
+		agents: []Agent{
+			&constantMoveAgent{change: Position{Y: 1, X: 0}},
+			&constantMoveAgent{change: Position{Y: 0, X: -1}},
+		},
+	}
 }
